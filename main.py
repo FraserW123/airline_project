05731d02b95b8e6d40b7bf359d402d621e065645
Fraser <fraser_wong@hotmail.com>
2024-11-19 18:08:08 -0800

    saved plotted image, implemented heatmap & correlation. Made simple classifier.

--- main.py
+++ main.py
@@ -11,40 +11,31 @@ from sklearn.preprocessing import OneHotEncoder
 
 import sklearn
 
-
-
-
-# Print the data
+####################################################
+# Load the data
 df = pd.read_csv("test.csv.xz", compression='xz')
-print(df.head())
+#print(df.head())
 
 # take a random sample of 1000 rows
-df = df.sample(n=1000)
-
-# Normalize the data
-
-# Select only numeric columns for scaling
-# numeric_cols = df.select_dtypes(include=[np.number]).columns
-# scaler = StandardScaler()
-# scaler.fit(df[numeric_cols])
-# df_normalized = scaler.transform(df[numeric_cols])
-
-
-# # Perform endcoding for the categorical data
-
-# encoder = OneHotEncoder()
-# encoder.fit(df)
-# df_encoded = encoder.transform(df)
+df = df.sample(n=10000)
 
 ontime = df[df['DEP_DEL15'] == 0]
 delayed = df[df['DEP_DEL15'] == 1]
 
-# Plot the columns of the ontime and delayed flights and compare, put all the plots in the same figure
 # %%
+####################################################
+# Inital DATA EXPLORATION
+df.shape
+df.info()
+df.describe()
 
+
+# %%
+####################################################
+# Inital EDA
+# Plot the columns of the ontime and delayed flights and compare, put all the plots in the same figure
 # Create a figure with 5 rows and 6 columns which are well spaced
 fig, axs = plt.subplots(5, 6, figsize=(20, 18))
-
 
 for i, col in enumerate(df.columns):
     ax = axs[i // 6, i % 6]
@@ -58,15 +49,33 @@ plt.savefig('flights.png')
 plt.show()
 
 
-    
-
-
 # %%
-print(ontime.shape)
-print(delayed.shape)
+# DATA CLEANING
+# encode the categorical data
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
 
-print(ontime.columns)
+def clean_labels_encoder(list_of_labels, df):
+    for label in list_of_labels:
+        df[label] = le.fit_transform(df[label])
+    return df
 
+# clean the labels
+list_of_labels = ['CARRIER_NAME', 'DEPARTING_AIRPORT', 'PREVIOUS_AIRPORT', 'DEP_TIME_BLK']
+df = clean_labels_encoder(list_of_labels, df)
+
+# Drop the columns that are not needed
+df = df.drop(['CARRIER_HISTORICAL', 'DEP_AIRPORT_HIST', 'DAY_HISTORICAL',
+       'DEP_BLOCK_HIST'], axis=1)
+
+# Fill the missing values
+df.fillna(df.mean(), inplace=True)
+
+
+# print(ontime.shape)
+# print(delayed.shape)
+
+# print(ontime.columns)
 """
 Index(['MONTH', 'DAY_OF_WEEK', 'DEP_DEL15', 'DEP_TIME_BLK', 'DISTANCE_GROUP',
        'SEGMENT_NUMBER', 'CONCURRENT_FLIGHTS', 'NUMBER_OF_SEATS',
@@ -79,8 +88,74 @@ Index(['MONTH', 'DAY_OF_WEEK', 'DEP_DEL15', 'DEP_TIME_BLK', 'DISTANCE_GROUP',
        'DEP_BLOCK_HIST'],
       dtype='object')
 """
+# plt.hist(ontime['MONTH'], alpha=0.5, label='ontime')
+# plt.hist(delayed['MONTH'], alpha=0.5, label='delayed')
 
 
+# %%
+####################################################
+# FEATURE ENGINEERING
+# Calculate the correlation between DEP_DEL15 and the other columns
+numeric_cols = df.select_dtypes(include=[np.number]).columns
+correlation = df[numeric_cols].corr()['DEP_DEL15']
+correlation = correlation.drop(['DEP_DEL15'])
 
+
+#print(correlation)
+
+# Plot the correlation
+plt.bar(correlation.index, correlation)
+plt.xticks(rotation=90)
+plt.tick_params(axis='x', rotation=90, labelsize=6)
+plt.ylabel('Correlation')
+plt.xlabel('Features')
+plt.title("Correlation between Delayed Flights and Features")
+plt.show()
+
+
+# Sort the correlation
+correlation = correlation.sort_values(ascending=False)
+print(correlation)
+
+# %%
+####################################################
+# Plot some heatmaps to see the correlation between the features
+import seaborn as sns
+# numeric_cols = numeric_cols.drop(['CARRIER_HISTORICAL', 'DEP_AIRPORT_HIST', 'DAY_HISTORICAL',
+#        'DEP_BLOCK_HIST'])
+
+def plot_heatmap(df):
+    plt.figure(figsize=(20, 10))
+    sns.set_theme(style='whitegrid', context='notebook')
+    cols = [0, 1, 2]
+    sns.heatmap(df.corr(), annot=True, square=False, cmap='coolwarm')
+    plt.title('Correlation between Features')
+    plt.show()
+
+plot_heatmap(df)
+
+# %%
+####################################################
+
+# Split the data into training and testing data
+from sklearn.model_selection import train_test_split
+
+X = df.drop('DEP_DEL15', axis=1)
+y = df['DEP_DEL15']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 
 # %%
